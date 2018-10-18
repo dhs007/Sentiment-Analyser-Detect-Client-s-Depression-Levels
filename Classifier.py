aafd4720f345da855a257c4a4e4b3c792094eca5
Darshil Shah <darshil98.shah@gmail.com>
2018-10-18 23:43:05 +0530

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -14,7 +14,7 @@ train = [
     ('I am tired of this stuff.', 'neg'),
     ("I can't deal with this", 'neg'),
     ('He is my sworn enemy!', 'neg'),
-    ('My boss is horrible.', 'neg')
+    ('My boss is horrible.', 'negative')
 ]
 test = [
     ('The beer was good.', 'pos'),
